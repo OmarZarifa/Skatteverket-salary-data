f3b6be857010a4014187b8a07624d29ce559d359
omar zarifa <omarzarifa@gmail.com>
2023-07-23 14:58:27 +0200

    initial commit

---
+++ src/main/java/com/skatteverket/salary_data/SalaryDataApplication.java
@@ -0,0 +1,13 @@
+package com.skatteverket.salary_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryDataApplication.class, args);
+	}
+
+}
